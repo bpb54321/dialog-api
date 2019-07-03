b67a8b7ab70608949c62b721a70f2f9f0c1d79f0
Brian Blosser <bpb54321@gmail.com>
2019-07-03 12:44:53 -0400

    Removed duplicate file

--- src/test/java/com/brianblosser/dialogapi/DialogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.brianblosser.dialogapi;
-
-import com.brianblosser.dialogapi.dialog.Dialog;
-import org.springframework.data.repository.CrudRepository;
-
-public interface DialogRepository extends CrudRepository<Dialog, Long> {
-}
